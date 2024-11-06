100b3c93da3da186cb0a46c8148304095b635156
Tap <46947072+ttappr@users.noreply.github.com>
2024-11-05 16:33:30 -0800

    The index type can be any numeric type to reduce memory usage.

--- src/suffix_array.rs
+++ src/suffix_array.rs
@@ -1,16 +1,16 @@
-use std::ops::{AddAssign, SubAssign, Sub, Add};
 
 // https://cp-algorithms.com/string/suffix-array.html
 
-// TODO - Make the type used as indices configurable so 8 bytes isn't used
-//        for every character of `s` 5 times over in `sort_cyclic_shifts()`.
+use std::ops::{AddAssign, SubAssign, Sub, Add};
+use std::fmt::Debug;
 
 const ALPHABET: usize = 256;
 
 fn sort_cyclic_shifts<T>(s: &str) -> Vec<T> 
 where
-    T: Add<Output=T> + AddAssign + Copy + Default + From<usize> + Into<usize> 
-        + PartialEq + Sub<Output=T> + SubAssign,
+    T: Add<Output=T> + AddAssign + Copy + Debug + Default + TryFrom<usize> 
+        + Into<usize> + PartialEq + Sub<Output=T> + SubAssign,
+    <T as TryFrom<usize>>::Error: Debug,
 {
     let n = s.len();
     let s = s.as_bytes();
@@ -22,94 +22,122 @@ where
     let mut classes = 1;
 
     for i in 0..n {
-        cnt[s[i] as usize] += 1.into();
+        cnt[s[i] as usize] += TryFrom::try_from(1).unwrap();
     }
     for i in 1..ALPHABET {
         cnt[i] = cnt[i] + cnt[i - 1];
     }
     for i in 0..n {
-        cnt[s[i] as usize] -= 1.into();
-        p[cnt[s[i] as usize].into()] = i.into();
+        cnt[s[i] as usize] -= TryFrom::try_from(1).unwrap();
+        p[cnt[s[i] as usize].into()] = TryFrom::try_from(i).unwrap();
     }
-    c[p[0].into()] = 0.into();
+    c[p[0].into()] = T::default();
 
     for i in 1..n {
         if s[p[i].into()] != s[p[i - 1].into()] { 
             classes += 1; 
         }
-        c[p[i].into()] = (classes - 1).into();
+        c[p[i].into()] = TryFrom::try_from(classes - 1).unwrap();
     }
 
     let mut h = 0;
 
     while (1 << h) < n {
-        h += 1;
         for i in 0..n {
             if p[i].into() >= (1 << h) {
-                pn[i] = (p[i].into() - (1 << h)).into();
+                pn[i] = TryFrom::try_from(p[i].into() - (1 << h)).unwrap();
             } else {
-                pn[i] = (p[i].into() + n - (1 << h)).into();
+                pn[i] = TryFrom::try_from(p[i].into() + n - (1 << h)).unwrap();
             }
         }
-        cnt[0..classes].fill(0.into());
+        cnt[0..classes].fill(T::default());
 
         for i in 0..n {
-            cnt[c[pn[i].into()].into()] += 1.into();
+            cnt[c[pn[i].into()].into()] += TryFrom::try_from(1).unwrap();
         }
         for i in 1..classes {
             cnt[i] = cnt[i] + cnt[i - 1];
         }
         for i in (0..n).rev() {
-            cnt[c[pn[i].into()].into()] -= 1.into();
+            cnt[c[pn[i].into()].into()] -= TryFrom::try_from(1).unwrap();
             p[cnt[c[pn[i].into()].into()].into()] = pn[i];
         }
-        cn[p[0].into()] = 0.into();
+        cn[p[0].into()] = T::default();
         classes = 1;
 
         for i in 1..n {
-            let curr = (c[p[i].into()], c[(p[i].into() + (1 << h)) % n]);
-            let prev = (c[p[i - 1].into()], c[(p[i - 1].into() + (1 << h)) % n]);
-            if curr == prev {
+            let curr = (c[p[i].into()], 
+                        c[(p[i].into() + (1 << h)) % n]);
+            let prev = (c[p[i - 1].into()], 
+                        c[(p[i - 1].into() + (1 << h)) % n]);
+            if curr != prev {
                 classes += 1;
             }
-            cn[p[i].into()] = (classes - 1).into();
+            cn[p[i].into()] = TryFrom::try_from(classes - 1).unwrap();
         }
         std::mem::swap(&mut c, &mut cn);
+        h += 1;
     }
     p
 }
 
-pub fn create_suffix_array(s: &mut String) -> Vec<usize> {
-    s.push('$');
+pub fn create_suffix_array<T>(s: &mut String) -> Vec<T> 
+where
+    T: Add<Output=T> + AddAssign + Copy + Debug + Default + TryFrom<usize> 
+        + Into<usize> + PartialEq + Sub<Output=T> + SubAssign,
+    <T as TryFrom<usize>>::Error: Debug,
+{
+    s.push('\0');
     let sa = sort_cyclic_shifts(&s)[1..].to_vec();
     s.pop();
     sa
 }
 
-pub fn create_lcp(s: &str, p: &[usize]) -> Vec<usize> {
+pub fn create_lcp<T>(s: &str, p: &[T]) -> Vec<T> 
+where
+    T: Add<Output=T> + AddAssign + Copy + Debug + Default + TryFrom<usize> 
+        + Into<usize> + PartialEq + Sub<Output=T> + SubAssign,
+    <T as TryFrom<usize>>::Error: Debug,
+{
     let n = s.len();
     let s = s.as_bytes();
-    let mut rank = vec![0; n];
-    let mut lcp = vec![0, n - 1];
+    let mut rank = vec![T::default(); n];
+    let mut lcp = vec![T::default(); n - 1];
     let mut k = 0;
 
     for i in 0..n {
-        rank[p[i]] = i;
+        rank[p[i].into()] = TryFrom::try_from(i).unwrap();
     }
     for i in 0..n {
-        if rank[i] == n - 1 {
+        if rank[i].into() == n - 1 {
             k = 0;
             continue;
         }
-        let j = p[rank[i] + 1];
+        let j = p[rank[i].into() + 1].into();
         while i + k < n && j + k < n && s[i + k] == s[j + k] {
             k += 1;
         }
-        lcp[rank[i]] = k;
+        lcp[rank[i].into()] = TryFrom::try_from(k).unwrap();
         if k > 0 {
             k -= 1;
         }
     }
 
     lcp
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let mut s = String::from("banana");
+        let sa: Vec<u8> = create_suffix_array(&mut s);
+        let lcp = create_lcp(&s, &sa);
+
+        assert_eq!(sa, vec![5, 3, 1, 0, 4, 2]);
+
+        assert_eq!(lcp, vec![1, 3, 0, 0, 2]);
+    }
 }
