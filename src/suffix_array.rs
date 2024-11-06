651c27ac2b028a8aa3f478d0837ed4049b34c7ca
Tap <46947072+ttappr@users.noreply.github.com>
2024-11-05 17:01:01 -0800

    Added document comments.

--- src/suffix_array.rs
+++ src/suffix_array.rs
@@ -1,11 +1,61 @@
-
-// https://cp-algorithms.com/string/suffix-array.html
+//! # Suffix array and LCP array construction.
+//! 
+//! The suffix array of a string is an array of integers that represents the 
+//! lexicographically sorted suffixes of the string. The LCP array of a string
+//! is an array of integers that represents the length of the longest common
+//! prefix between consecutive suffixes in the suffix array.
+//! 
+//! The functions of this module are designed to reduce their memory footprints.
+//! If, for example, the target string is only 10,000 bytes long, the functions 
+//! can internally use `u16` instead of `usize` for their temporary arrays. The 
+//! callers must ensure that the type used can represent the largest **byte** 
+//! index of the target string.
+//! 
+//! 
+//! ## Complexity
+//! - Construction of the suffix array:
+//!     - Time complexity: O(n log n)
+//!     - Space complexity: O(n)
+//! - Construction of the LCP array:
+//!    - Time complexity: O(n)
+//!    - Space complexity: O(n)
+//! 
+//! ## Examples
+//! ```
+//! use suffix_array::*;
+//! 
+//! let mut s = String::from("banana");
+//! let sa = create_suffix_array::<u8>(&mut s);
+//! let lcp = create_lcp(&s, &sa);
+//! 
+//! assert_eq!(sa, vec![5, 3, 1, 0, 4, 2]);
+//! assert_eq!(lcp, vec![1, 3, 0, 0, 2]);
+//! ```
+//! 
+//! ## References
+//! - [CP-Algorithms](https://cp-algorithms.com/string/suffix-array.html) 
+//!   provides an implementation of the suffix array and LCP array construction
+//!   algorithms in C++. The implementation in this module is based on the
+//!   implementation provided in the link.
 
 use std::ops::{AddAssign, SubAssign, Sub, Add};
 use std::fmt::Debug;
 
 const ALPHABET: usize = 256;
 
+/// Constructs arrays and performs the sorting of suffix indices for the given 
+/// string.
+/// 
+/// # Arguments
+/// * `s`: A mutable reference to a string.
+/// 
+/// # Generic Types
+/// * `T`: The type of the elements in the suffix array. Expected to be one of
+///        the primitive integer types.
+/// 
+/// # Returns
+/// A vector of integers representing the suffix array of the string.
+/// 
 fn sort_cyclic_shifts<T>(s: &str) -> Vec<T> 
 where
     T: Add<Output=T> + AddAssign + Copy + Debug + Default + TryFrom<usize> 
@@ -81,6 +131,14 @@ where
     p
 }
 
+/// Constructs the suffix array of a string.
+/// 
+/// # Arguments
+/// * `s`: A string.
+/// 
+/// # Returns
+/// A vector of integers representing the sorted suffixes of the string.
+/// 
 pub fn create_suffix_array<T>(s: &mut String) -> Vec<T> 
 where
     T: Add<Output=T> + AddAssign + Copy + Debug + Default + TryFrom<usize> 
@@ -93,6 +151,15 @@ where
     sa
 }
 
+/// Constructs the LCP array of a string.
+/// 
+/// # Arguments
+/// * `s`: A string.
+/// * `p`: A reference to the suffix array of the string.
+/// 
+/// # Returns
+/// A vector of integers representing the LCP array of the string.
+/// 
 pub fn create_lcp<T>(s: &str, p: &[T]) -> Vec<T> 
 where
     T: Add<Output=T> + AddAssign + Copy + Debug + Default + TryFrom<usize> 
@@ -133,11 +200,11 @@ mod tests {
     #[test]
     fn test_1() {
         let mut s = String::from("banana");
-        let sa: Vec<u8> = create_suffix_array(&mut s);
+
+        let sa  = create_suffix_array::<u8>(&mut s);
         let lcp = create_lcp(&s, &sa);
 
-        assert_eq!(sa, vec![5, 3, 1, 0, 4, 2]);
-
+        assert_eq!(sa,  vec![5, 3, 1, 0, 4, 2]);
         assert_eq!(lcp, vec![1, 3, 0, 0, 2]);
     }
 }
